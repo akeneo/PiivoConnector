407a5e79d6a5eb2c071a137c8205aa6e07ea9de4
romain <romain@akeneo.com>
2017-04-24 14:11:44 +0200

    Rename bundle

---
+++ src/PiivoConnectorBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Piivo\Bundle\ConnectorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ */
+class PiivoConnectorBundle extends Bundle
+{
+}
